27a6a4bbefcd75719055a693eccd65d1319a1eb0
Ceora Ford <ceoraallthetime@gmail.com>
2018-07-24 14:53:14 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -22,5 +22,5 @@ def bob_the_reins
   puts " Lift your arms up and down in a loose bouncing movement, in time with the beat of the song"
 end
 def lasso 
-  puts ""
+  puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
 end 
