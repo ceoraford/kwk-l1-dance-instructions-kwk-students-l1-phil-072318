bf01d742a037c1887effaf89e1187869a3a0621a
Ceora Ford <ceoraallthetime@gmail.com>
2018-07-24 14:52:41 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,26 @@
 
 # Code your methods below
+def assume_stance 
+  puts "Plant legs far apart, bend knees slightly and keep posture loose"
+end
+def base_footwork
+  puts "Lift right foot"
+  puts "Return right foot to the ground"
+  puts "Finishing with a small skip-step backward"
+end 
+def skip_step
+  puts "Lower left foot to the ground"
+  puts "Bounce left foot back up slightly, kicking a few inches back"
+  puts "Left, Right, Left and Left"
+  puts "Lower right foot to the ground"
+  puts "Bounce right foot back up slightly, kicking a few inches back"
+  puts "Right, Left, Right and Right"
+end
+def bob_the_reins
+  puts "Holding your arms out in front of you, straight and at chest level"
+  puts "Cross your right wrist over your left and hold them together"
+  puts " Lift your arms up and down in a loose bouncing movement, in time with the beat of the song"
+end
+def lasso 
+  puts"
+end 
